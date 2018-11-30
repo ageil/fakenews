41c742550328a46dfa062cdfa68df49f4d1cd75c
Anders Geil <anders.geil@gmail.com>
2018-11-30 11:47:19 -0800

    renamed parameters, added output comments

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 # Number of agents in the model
 N = 3
 
-# Number of iterations
-timesteps = 4
+# Number of time steps to simulate
+T = 4
 
 # create network
 network = nx.complete_graph(N)
@@ -16,8 +16,8 @@ network = nx.complete_graph(N)
 # create model
 model = KnowledgeModel(network)
 
-for timestep in range(timesteps):
-    print("\n~~~~~ ROUND " + str(timestep) + " ~~~~~\n")
+for t in range(T):
+    print("\n~~~~~ ROUND " + str(t) + " ~~~~~\n")
     for agent in model.schedule.agents:
         print(agent.unique_id, agent.belief)
     model.step()
@@ -26,6 +26,6 @@ for timestep in range(timesteps):
 beliefs, interactions, pairs = model.logs()
 df_belief = pd.DataFrame.from_dict(beliefs)
 
-print(df_belief)   # columns = ID, rows = belief at timestep
-print(interactions)   # id: [(interlocutor0_id, interlocutor0_belief), (interlocutor1_id, interlocutor1_belief), ...]
-print(pairs)
+print(df_belief)     # columns = ID, rows = belief at timestep
+print(interactions)  # id: [(interlocutor0_id, interlocutor0_belief), (interlocutor1_id, interlocutor1_belief), ...]
+print(pairs)         # [{agentA1, agentB1}, {agentA2, agentB2}, ..., {agentAT, agentBT}]
