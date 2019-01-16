bca6c6f58b72db9f744569639e047ce91abde13c
Anders Geil <anders.geil@gmail.com>
2019-01-15 17:34:40 -0800

    Added support for agent belief share limit; ran simulations with restricted number of shares

--- agent.py
+++ agent.py
@@ -5,22 +5,27 @@ from belief import Belief
 class PopAgent(Agent):
     """An Agent with some initial knowledge."""
 
-    def __init__(self, unique_id, model, neighbors):
+    def __init__(self, unique_id, model, neighbors, num_shares):
         super().__init__(unique_id, model)
 
         # Default params
         self.belief = Belief.Neutral
         self.neighbors = neighbors  # list of agent's neighbours
+        self.num_shares = num_shares
 
     def update(self, other):
         """Update agent's own beliefs"""
+        isSharing = other.num_shares > 0
+
         # Convert self to false belief
-        if self.belief == Belief.Neutral and other.belief == Belief.Fake:
+        if self.belief == Belief.Neutral and other.belief == Belief.Fake and isSharing:
             self.belief = Belief.Fake
+            other.num_shares -= 1
 
         # Convert self to retracted belief
-        if self.belief == Belief.Fake and other.belief == Belief.Retracted:
+        if self.belief == Belief.Fake and other.belief == Belief.Retracted and isSharing:
             self.belief = Belief.Retracted
+            other.num_shares -= 1
 
     def step(self, interlocutor):
         """Interact with interlocutor, updating own beliefs."""
--- main.py
+++ main.py
@@ -6,22 +6,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Hyperparameters:
-S = 1000  # Number of simulations to run
-T = 100  # Number of time steps per simulation
-N = 10   # Number of agents in the model
+N = 100    # Number of agents in the model
+T = 1000   # Number of time steps per simulation
+S = 1000   # Number of simulations to run
+num_shares = np.infty  # Number of times an agent will share their non-neutral belief; set np.infty for unlimited
 network = nx.complete_graph(N)   # Agent network
 network.name = "complete"        # Network type used for output
+plot_sd = False  # show standard deviation on output plot
 
 
-def runModel(network, T, debug=False):
+def runModel(network, T):
     # create model
-    model = KnowledgeModel(network)
+    model = KnowledgeModel(network=network, num_shares=num_shares)
 
     for t in range(T-1):
-        if debug:
-            print("\n~~~~~ ROUND " + str(t) + " ~~~~~\n")
-            for agent in model.schedule.agents:
-                print(agent.unique_id, agent.belief)
         model.step()
 
     return model.logs()
@@ -47,25 +45,42 @@ def runSimulation(S, T, network):
 
     avg_num_fake_per_agent = np.mean(num_fake_per_agent)
     frac_fake_per_timestep = np.mean(fake_per_timestep, axis=0)
+    frac_fake_per_timestep_sd = np.std(fake_per_timestep, axis=0)
     frac_retracted_per_timestep = np.mean(retracted_per_timestep, axis=0)
+    frac_retracted_per_timestep_sd = np.std(retracted_per_timestep, axis=0)
     frac_neutral_per_timestep = np.mean(neutral_per_timestep, axis=0)
+    frac_neutral_per_timestep_sd = np.std(neutral_per_timestep, axis=0)
 
-    return avg_num_fake_per_agent, frac_fake_per_timestep, frac_retracted_per_timestep, frac_neutral_per_timestep
+    frac_belief_mean = (frac_neutral_per_timestep, frac_fake_per_timestep, frac_retracted_per_timestep)
+    frac_belief_sd = (frac_neutral_per_timestep_sd, frac_fake_per_timestep_sd, frac_retracted_per_timestep_sd)
+
+    return avg_num_fake_per_agent, frac_belief_mean, frac_belief_sd
 
 
-avg_num_fake, frac_fake, frac_rtrt, frac_ntrl = runSimulation(S, T, network)
+avg_num_fake, frac_belief_mean, frac_belief_sd = runSimulation(S, T, network)
+neutral_mean, fake_mean, retracted_mean = frac_belief_mean
+neutral_sd, fake_sd, retracted_sd = frac_belief_sd
 print("Average number of time steps holding false belief:", avg_num_fake)
 
-plt.plot(range(T), frac_fake, label="False", color="tab:red")
-plt.plot(range(T), frac_ntrl, label="Neutral", color="tab:orange")
-plt.plot(range(T), frac_rtrt, label="Retracted", color="tab:green")
+alpha = 0.5
+plt.plot(range(T), fake_mean, label="False", color="tab:red", ls="-")
+plt.plot(range(T), neutral_mean, label="Neutral", color="tab:orange", ls="-")
+plt.plot(range(T), retracted_mean, label="Retracted", color="tab:green", ls="-")
+if plot_sd:
+    plt.plot(range(T), fake_mean+fake_sd, color="tab:red", ls="--", alpha=alpha)
+    plt.plot(range(T), fake_mean-fake_sd, color="tab:red", ls="--", alpha=alpha)
+    plt.plot(range(T), neutral_mean+neutral_sd, color="tab:orange", ls="--", alpha=alpha)
+    plt.plot(range(T), neutral_mean-neutral_sd, color="tab:orange", ls="--", alpha=alpha)
+    plt.plot(range(T), retracted_mean+retracted_sd, color="tab:green", ls="--", alpha=alpha)
+    plt.plot(range(T), retracted_mean-retracted_sd, color="tab:green", ls="--", alpha=alpha)
 plt.xlim(0,T)
 plt.ylim(0,1.11)
 plt.xlabel("Time")
 plt.ylabel("Proportion of population holding belief")
-plt.title("N = {N}, T = {T}, S = {S}, Num = {avg}".format(N=N, T=T, S=S, avg=round(avg_num_fake, 1)))
+plt.title("N = {N}, T = {T}, S = {S}, Num = {avg}, Shares = {shr}".format(N=N, T=T, S=S, avg=round(avg_num_fake, 1), shr=num_shares))
 plt.legend(loc="lower center", ncol=3, fancybox=True, bbox_to_anchor=(0.5, 0.9))
-plt.savefig("./output/S{S}-T{T}-N{N}-{avg}-{name}.png".format(S=S, T=T, N=N, avg=round(avg_num_fake,1), name=network.name), bbox_inches="tight")
+plt.savefig("./output/N{N}-T{T}-S{S}-{shr}-{name}-{avg}.png".format(N=N, T=T, S=S, shr=num_shares, name=network.name,
+                                                                    avg=round(avg_num_fake,1)), bbox_inches="tight")
 plt.show()
 
 
